111414305a825107986bee6a715ad26cee7d34a3
po <per-olof.markstrom@footway.com>
2025-07-16 22:19:13 +0200

    expand item model with additional metafields and update front end to match

--- upkeep/models.py
+++ upkeep/models.py
@@ -19,11 +19,29 @@ class Location(BaseModel):
 
 
 class Item(BaseModel):
+    class ItemStatus(models.IntegerChoices):
+        ACTIVE = 1, "Active"
+        RETIRED = 2, "Retired"
+        BROKEN = 3, "Broken"
+
+    # Mandatory
     name = models.CharField(max_length=255)
     location = models.ForeignKey(
         Location, null=True, on_delete=models.SET_NULL, related_name="items"
     )
-    sub_location = models.CharField(max_length=30, blank=True, null=True)
+    status = models.IntegerField(
+        choices=ItemStatus, null=False, default=ItemStatus.ACTIVE
+    )
+    quantity = models.PositiveIntegerField(default=1)
+
+    # Optional metadata
+    area = models.CharField(max_length=30, blank=True, null=True)
+    brand = models.CharField(max_length=255, blank=True, null=True)
+    model_number = models.CharField(max_length=255, blank=True, null=True)
+    serial_number = models.CharField(max_length=255, blank=True, null=True)
+
+    purchase_value = models.PositiveIntegerField(null=True, blank=True)
+    purchase_place = models.CharField(max_length=255, blank=True, null=True)
     purchase_year = models.PositiveIntegerField(
         null=True,
         blank=True,
@@ -32,13 +50,31 @@ class Item(BaseModel):
             MaxValueValidator(datetime.datetime.now().year + 1),
         ],
     )
-    initial_value = models.PositiveIntegerField(null=True, blank=True)
+    warranty_expiration = models.DateField(blank=True, null=True)
+    receipt_file = models.FileField(upload_to="receipts/", blank=True, null=True)
+    notes = models.TextField(blank=True, null=True)
+    manufacturer_manual_url = models.URLField(null=True, blank=True)
+
+    end_of_service_date = models.DateField(blank=True, null=True)
 
     class Meta:
         verbose_name_plural = "Items"
 
     def __str__(self):
         return self.name
+
+    def is_under_warranty(self):
+        return self.warranty_expiration and self.warranty_expiration >= datetime.date()
+
+    def get_status_badge_class(self):
+        return {
+            self.ItemStatus.ACTIVE: "success",
+            self.ItemStatus.RETIRED: "warning",
+            self.ItemStatus.BROKEN: "danger",
+        }.get(self.status, "secondary")
+
+    def get_status_display_name(self):
+        return self.ItemStatus(self.status).label
 
 
 class Task(BaseModel):
